99bcff8557825d52d38612864b20dfc1180aa9bb
Josh Overly and Marika Allely <josh@>
2016-03-29 14:02:11 -0700

    add content to story details page

--- app/components/story-tile.js
+++ app/components/story-tile.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    delete(story) {
-      if(confirm('Are sure you want to kill this bitch?!')) {
-        this.sendAction('destroyStory', story);
-      }
-    }
-  }
+  // actions: {
+  //   delete(story) {
+  //     if(confirm('Are sure you want to kill this bitch?!')) {
+  //       this.sendAction('destroyStory', story);
+  //     }
+  //   }
+  // }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,10 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('story');
   },
-  actions: {
-    destroyStory(story) {
-      story.destroyRecord();
-      this.transitionTo('index');
-    }
-  }
+  // actions: {
+  //   destroyStory(story) {
+  //     story.destroyRecord();
+  //     this.transitionTo('index');
+  //   }
+  // }
 });
